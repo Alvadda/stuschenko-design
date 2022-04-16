87b70883c5114f272a1081dc1a38957fa4fc7681
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-04-16 12:12:13 +0200

    add text and h2 to global styles

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -7,12 +7,12 @@ function Landing() {
       <div className={style.img} />
       <div>
         <h2 className={style.heading}>Welcome!</h2>
-        <p className={style.text}>I`m Johanna, a Webdesigner from Hamburg, Germany</p>
-        <p className={style.text}>
+        <p className="text">I`m Johanna, a Webdesigner from Hamburg, Germany</p>
+        <p className="text">
           I am right at the beginning of my career as a webdesigner and are pleasent to welcoming you at my website I am currently
           designing.
         </p>
-        <p className={style.text}>Get in touch with my projects</p>
+        <p className="text">Get in touch with my projects</p>
         <Button variant="full">see more</Button>
       </div>
     </div>
