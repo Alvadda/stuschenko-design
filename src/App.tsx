1ad76351d4e28481e09ad6611fc7673fdd1ec8a3
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-04-16 11:26:25 +0200

    add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Landing from './pages/landing/Landing'
 
 function App() {
-  return <div>Hallo</div>
+  return (
+    <div>
+      <Header />
+      <Landing />
+    </div>
+  )
 }
 
 export default App
