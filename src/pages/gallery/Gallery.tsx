efcefd5c5907b073b9c33c6d64ee51ac3bdf9297
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-04-17 17:37:49 +0200

    add lazyImg to gallery

--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -1,3 +1,4 @@
+import LazyImg from '../../components/lazyImg/LazyImg'
 import Page from '../../components/page/Page'
 import style from './gallery.module.css'
 
@@ -5,16 +6,36 @@ function Gallery() {
   return (
     <Page page="about" threshold={0.6}>
       <div className={style.container}>
-        <div className={style.img0}></div>
-        <div className={style.img1}></div>
-        <div className={style.img2}></div>
-        <div className={style.img3}></div>
-        <div className={style.img4}></div>
-        <div className={style.img5}></div>
-        <div className={style.img6}></div>
-        <div className={style.img7}></div>
-        <div className={style.img8}></div>
-        <div className={style.img9}></div>
+        <div className={style.img0}>
+          <LazyImg alt="todo" src="" />
+        </div>
+        <div className={style.img1}>
+          <LazyImg alt="todo" src="" />
+        </div>
+        <div className={style.img2}>
+          <LazyImg alt="todo" src="" />
+        </div>
+        <div className={style.img3}>
+          <LazyImg alt="todo" src="" />
+        </div>
+        <div className={style.img4}>
+          <LazyImg alt="todo" src="" />
+        </div>
+        <div className={style.img5}>
+          <LazyImg alt="todo" src="" />
+        </div>
+        <div className={style.img6}>
+          <LazyImg alt="todo" src="" />
+        </div>
+        <div className={style.img7}>
+          <LazyImg alt="todo" src="" />
+        </div>
+        <div className={style.img8}>
+          <LazyImg alt="todo" src="" />
+        </div>
+        <div className={style.img9}>
+          <LazyImg alt="todo" src="" />
+        </div>
       </div>
     </Page>
   )
