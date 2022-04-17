4db23e115246679cdb44fd362c7f4dcc1db1acde
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-04-17 18:56:31 +0200

    add img cover to div

--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -7,34 +7,64 @@ function Gallery() {
     <Page page="about" threshold={0.6}>
       <div className={style.container}>
         <div className={style.img0}>
-          <LazyImg alt="todo" src="" />
+          <LazyImg
+            alt="cute cat"
+            src="https://images.unsplash.com/photo-1624375540566-877eecad23d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          />
         </div>
         <div className={style.img1}>
-          <LazyImg alt="todo" src="" />
+          <LazyImg
+            alt="cute cat"
+            src="https://images.unsplash.com/photo-1624375540566-877eecad23d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          />
         </div>
         <div className={style.img2}>
-          <LazyImg alt="todo" src="" />
+          <LazyImg
+            alt="cute cat"
+            src="https://images.unsplash.com/photo-1624375540566-877eecad23d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          />
         </div>
         <div className={style.img3}>
-          <LazyImg alt="todo" src="" />
+          <LazyImg
+            alt="cute cat"
+            src="https://images.unsplash.com/photo-1624375540566-877eecad23d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          />
         </div>
         <div className={style.img4}>
-          <LazyImg alt="todo" src="" />
+          <LazyImg
+            alt="cute cat"
+            src="https://images.unsplash.com/photo-1624375540566-877eecad23d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          />
         </div>
         <div className={style.img5}>
-          <LazyImg alt="todo" src="" />
+          <LazyImg
+            alt="cute cat"
+            src="https://images.unsplash.com/photo-1624375540566-877eecad23d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          />
         </div>
         <div className={style.img6}>
-          <LazyImg alt="todo" src="" />
+          <LazyImg
+            alt="cute cat"
+            src="https://images.unsplash.com/photo-1624375540566-877eecad23d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          />
         </div>
         <div className={style.img7}>
-          <LazyImg alt="todo" src="" />
+          <LazyImg
+            alt="cute cat"
+            src="https://images.unsplash.com/photo-1624375540566-877eecad23d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          />
         </div>
         <div className={style.img8}>
-          <LazyImg alt="todo" src="" />
+          <LazyImg
+            alt="cute cat"
+            src="https://images.unsplash.com/photo-1624375540566-877eecad23d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          />
         </div>
         <div className={style.img9}>
-          <LazyImg alt="todo" src="" />
+          <LazyImg
+            alt="cute cat"
+            src="https://images.unsplash.com/photo-1624375540566-877eecad23d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          />
         </div>
       </div>
     </Page>
