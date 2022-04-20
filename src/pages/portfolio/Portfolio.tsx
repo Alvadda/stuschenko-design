8352bcf009c27344c843362af78c16d6101f1612
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-04-20 08:29:13 +0200

    add carousel to portfolio

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -1,3 +1,4 @@
+import Carousel from '../../components/carousel/Carousel'
 import Page from '../../components/page/Page'
 import style from './portfolio.module.css'
 
@@ -6,6 +7,12 @@ function Portfolio() {
     <Page page="portfolio" threshold={0.6}>
       <div className={style.container}>
         <h2>PORTFOLIO</h2>
+        <Carousel>
+          <div style={{ backgroundColor: 'green' }}></div>
+          <div style={{ backgroundColor: 'pink' }}></div>
+          <div style={{ backgroundColor: 'blue' }}></div>
+          <div style={{ backgroundColor: 'red' }}></div>
+        </Carousel>
       </div>
     </Page>
   )
