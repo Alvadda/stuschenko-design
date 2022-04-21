5ea46627cd0fdd2a21fc61ca5dc55ddcb3febc0c
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-04-21 08:15:56 +0200

    add portfolio page content

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -5,7 +5,39 @@ function Portfolio() {
   return (
     <Page page="portfolio" threshold={0.6}>
       <div className={style.container}>
-        <h2>Portfolio</h2>
+        <div>
+          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+          <p className="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac blandit eget vitae sapien. Urna placerat sagittis, enim non sodales
+            proin. Curabitur nunc arcu risus porttitor. Arcu quis gravida nisl, hac vivamus vulputate purus parturient.
+          </p>
+        </div>
+        <div className={style.portfolios}>
+          <div className={style.portfolio}>
+            <div className={style.img} />
+            <div className={style.textContainer}>
+              <h3>TT App</h3>
+              <p className="text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet morbi enim tincidunt viverra vitae quis nisl.
+              </p>
+              <p className="text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet morbi enim tincidunt viverra vitae quis nisl
+              </p>
+            </div>
+          </div>
+          <div className={style.portfolio}>
+            <div className={style.img} />
+            <div className={style.textContainer}>
+              <h3>STUSCHENKO_DESIGN.COM</h3>
+              <p className="text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet morbi enim tincidunt viverra vitae quis nisl.
+              </p>
+              <p className="text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet morbi enim tincidunt viverra vitae quis nisl
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </Page>
   )
