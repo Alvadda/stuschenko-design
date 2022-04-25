bd9b0f3c37220f824b145a69cbad7e254f3e7fe4
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-04-25 07:53:28 +0200

    redesign about

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,9 +5,9 @@ import style from './about.module.css'
 function About() {
   return (
     <Page page="about" threshold={0.6}>
-      <div className={style.container}>
+      <div className="container">
         <h2>How did I become a webdesigner?</h2>
-        <div className={style.content}>
+        <div className="content">
           <div className={style.img} />
           <div>
             <p className="text">
@@ -16,16 +16,17 @@ function About() {
             </p>
             <p className="text">
               I made an apprenticeship as a hotel specialist and had no previous experience in this industry so it was a bit tricky for
-              myself. In Summer 2021 I started educationg myself and I am still in porgress of learning.
+              myself. In Summer 2021 I started educationg myself and I am still in porgress of learning. At preasent I am doing an the
+              goople online course “Foundations of User Experience (UX) Design”.
             </p>
             <p className="text">
-              In this project we both did designing so I am very exided that I decided in spring 2022 to design my own website. This project
-              is the first designed all by myself.
+              While desiging this app my boyfriend and I did designing together so I am very exided that I am designing my very own website
+              in spring 2022.
             </p>
-            <p className="text">If you`re asking how I stayed motivated get in contact with me!</p>
-            <div className={style.buttons}>
-              <Button variant="full">Get in touch</Button>
-              <Button variant="outlined">learn more about me</Button>
+            <p className="text">Want to see my projects or leran more about me? Press one of the buttons to find out!</p>
+            <div className="buttonsGrp">
+              <Button variant="full">My projects</Button>
+              <Button variant="outlined">About me</Button>
             </div>
           </div>
         </div>
