4c8560b011cff9b50e90a8810883d06eec903ce7
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-04-25 07:32:35 +0200

    redesign homepage

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -6,16 +6,21 @@ function Landing() {
   return (
     <Page page="home" threshold={0.5}>
       <div className={style.container}>
-        <div className={style.img} />
-        <div>
-          <h2 className={style.heading}>Welcome!</h2>
-          <p className="text bold">I`m Johanna, a Webdesigner from Hamburg, Germany</p>
-          <p className="text bold">
-            I am right at the beginning of my career as a webdesigner and are pleasent to welcoming you at my website I am currently
-            designing.
-          </p>
-          <p className="text bold">Get in touch with my projects</p>
-          <Button variant="full">see more</Button>
+        <h2 className={style.heading}>Welcome!</h2>
+        <div className={style.content}>
+          <div className={style.img} />
+          <div>
+            <p className="text">I`m Johanna, a Webdesigner from Hamburg, Germany</p>
+            <p className="text">
+              I am right at the beginning of my career as a webdesigner and are pleasent to welcoming you at my website I am currently
+              designing.
+            </p>
+            <p className="text">Want to leran more about me or my Projects? Press the button to find out!</p>
+            <div className={style.buttons}>
+              <Button variant="full">About me</Button>
+              <Button variant="outlined">My projects</Button>
+            </div>
+          </div>
         </div>
       </div>
     </Page>
