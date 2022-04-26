10e01b0c1b4ab3b2416cc3e4eccb9e02bd370770
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-04-26 07:37:08 +0200

    update portfolio

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -5,36 +5,26 @@ function Portfolio() {
   return (
     <Page page="portfolio" threshold={0.6}>
       <div className={style.container}>
-        <div>
-          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac blandit eget vitae sapien. Urna placerat sagittis, enim non sodales
-            proin. Curabitur nunc arcu risus porttitor. Arcu quis gravida nisl, hac vivamus vulputate purus parturient.
-          </p>
-        </div>
+        <h2>My previous projects</h2>
         <div className={style.portfolios}>
           <div className={style.portfolio}>
             <div className={style.img} />
             <div className={style.textContainer}>
-              <h3>TT App</h3>
-              <p className="text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet morbi enim tincidunt viverra vitae quis nisl.
-              </p>
-              <p className="text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet morbi enim tincidunt viverra vitae quis nisl
-              </p>
+              <h3 className={style.portfolioTitle}>TT App</h3>
+              <p className="text">You can track your time, add projects, tasks and some notes.</p>
+              <p className="text">Later on you are also able to get an invoice and will be able to see your time sheet.</p>
+              <p className="text">You can change white- and darkmode as well as language. </p>
             </div>
           </div>
           <div className={style.portfolio}>
             <div className={style.img} />
-            <div className={style.textContainer}>
-              <h3>STUSCHENKO_DESIGN.COM</h3>
+            <div>
+              <h3 className={style.portfolioTitle}>STUSCHENKO_DESIGN.COM</h3>
               <p className="text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet morbi enim tincidunt viverra vitae quis nisl.
+                I wanted to create a website where you can get to know my work as well as me and my personality and will continuous update
+                my website.{' '}
               </p>
-              <p className="text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet morbi enim tincidunt viverra vitae quis nisl
-              </p>
+              <p className="text">A website that convince, a website that after you had a look you want to meet that person. </p>
             </div>
           </div>
         </div>
