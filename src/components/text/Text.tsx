3a956ede8a8aaf30211679590472f0cb5f583001
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-05-22 17:00:48 +0200

    add textalign to text component

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -5,13 +5,19 @@ import style from './text.module.css'
 interface Props {
   type?: 'h1' | 'h2' | 'h3' | 'p' | 'a'
   color?: 'dark' | 'light'
+  textAlign?: 'start' | 'center' | 'end'
   children?: ReactNode
 }
 
-function Text({ type = 'p', color = 'dark', children }: Props) {
+function Text({ type = 'p', color = 'dark', textAlign = 'start', children }: Props) {
   if (type === 'h2') return <h2 className={style.h2}>{children}</h2>
+  if (type === 'h3') return <h3 className={style.h3}>{children}</h3>
 
-  return <p className={style.p}>{children}</p>
+  return (
+    <p className={style.p} style={{ textAlign }}>
+      {children}
+    </p>
+  )
 }
 
 export default Text
