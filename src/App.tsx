3ff611ea784f267d751b3ff9c9511890f806224b
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-05-22 16:11:57 +0200

    change about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
 import React from 'react'
+
 import Header from './components/header/Header'
 import PageContextProvider from './contexts/PageContext'
 import About from './pages/about/About'
-import Apprenticeship from './pages/about/Apprenticeship'
-import Australia from './pages/about/Australia'
-import AustraliaV2 from './pages/about/AustraliaV2'
-import Childhood from './pages/about/Childhood'
 import Contact from './pages/contact/Contact'
-import Gallery from './pages/gallery/Gallery'
-import Landing from './pages/landing/Landing'
 import Portfolio from './pages/portfolio/Portfolio'
+import Welcome from './pages/welcome/welcome'
 
 function App() {
   return (
     <PageContextProvider>
       <Header />
-      <Landing />
+      <Welcome />
       <About />
-      <Childhood />
-      <Apprenticeship />
-      <Australia />
-      <AustraliaV2 />
-      <Gallery />
       <Portfolio />
       <Contact />
     </PageContextProvider>
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,36 +1,44 @@
+import about1Img from '../../assets/about1.jpeg'
+import about2Img from '../../assets/about2.jpeg'
+import about3Img from '../../assets/about3.jpg'
+import about4Img from '../../assets/about4.jpg'
+import about5Img from '../../assets/about5.jpg'
 import Button from '../../components/button/Button'
+import ButtonGrp from '../../components/button/ButtonGrp'
+import Carousel from '../../components/carousel/Carousel'
+import Layout from '../../components/layout/Layout'
+import LazyImg from '../../components/lazyImg/LazyImg'
 import Page from '../../components/page/Page'
-import style from './about.module.css'
+import Text from '../../components/text/Text'
 
 function About() {
   return (
     <Page page="about" threshold={0.6}>
-      <div className="container">
-        <h2>How did I become a webdesigner?</h2>
-        <div className="content">
-          <div className={style.img} />
-          <div>
-            <p className="text">
-              Not long ago a friend intended that there is no good app for time tracking for freelancer so my friend and I decided to
-              develope.
-            </p>
-            <p className="text">
-              I made an apprenticeship as a hotel specialist and had no previous experience in this industry so it was a bit tricky for
-              myself. In Summer 2021 I started educationg myself and I am still in porgress of learning. At preasent I am doing an the
-              goople online course “Foundations of User Experience (UX) Design”.
-            </p>
-            <p className="text">
-              While desiging this app my boyfriend and I did designing together so I am very exided that I am designing my very own website
-              in spring 2022.
-            </p>
-            <p className="text">Want to see my projects or leran more about me? Press one of the buttons to find out!</p>
-            <div className="buttonsGrp">
-              <Button variant="full">My projects</Button>
-              <Button variant="outlined">About me</Button>
-            </div>
-          </div>
+      <Layout title="about">
+        <Carousel speed={8000} progressBar height="51.2rem">
+          <LazyImg alt="Johanna Stuschenko as child" src={about1Img} />
+          <LazyImg alt="Confirmation of Johanna Stuschenko" src={about2Img} />
+          <LazyImg alt="Johanna Stuschenko with a big backpack Australia" src={about3Img} />
+          <LazyImg alt="Johanna Stuschenko smiling in front of a bush" src={about4Img} />
+          <LazyImg alt="Johanna Stuschenko smiling in front of a sea" src={about5Img} />
+        </Carousel>
+        <div>
+          <Text>
+            I grew up in a small town and moved for my apprenticeship to Hamburg. After finishing, I deiced to go to Australia for a work
+            and travel year.
+            <br />
+            <br /> One year in Australia was great, but I wanted to move back to Hamburg, Germany. I meet my boyfriend Christian, he is the
+            person who established software engineering and web design into my world. Since summer 2021 I am education myself and still
+            finding out what part I will be specializing in. At present, I am doing an online course from Google Foundations of User
+            Experience (UX) Design.
+            <br />
+            <br /> I will continually update this website and am very excited to welcoming you here! Come and get in contact with me!
+          </Text>
+          <ButtonGrp>
+            <Button variant="full">Contact me</Button>
+          </ButtonGrp>
         </div>
-      </div>
+      </Layout>
     </Page>
   )
 }
