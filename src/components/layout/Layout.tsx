895624d5c5f78d2b91219098f7ede16242039541
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-05-22 17:00:09 +0200

    change portfolio

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,22 @@
 import { ReactNode } from 'react'
 
+import { combineClassNames } from '../../utils/utils'
 import Text from '../text/Text'
 import style from './layout.module.css'
 
 interface Props {
   title: string
+  contentSpacing?: 'm' | 'l'
   children?: ReactNode
 }
 
-function Layout({ title, children }: Props) {
+function Layout({ title, contentSpacing = 'l', children }: Props) {
+  const spacingClass = contentSpacing === 'l' ? style.spacingLarge : style.spacingSmall
+
   return (
     <div className={style.container}>
       <Text type="h2">{title}</Text>
-      <div className={style.content}>{children}</div>
+      <div className={combineClassNames([style.content, spacingClass])}>{children}</div>
     </div>
   )
 }
--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -1,34 +1,30 @@
+import Layout from '../../components/layout/Layout'
 import Page from '../../components/page/Page'
-import style from './portfolio.module.css'
+import Stack from '../../components/stack/Stack'
+import Text from '../../components/text/Text'
 
 function Portfolio() {
   return (
     <Page page="portfolio" threshold={0.6}>
-      <div className={style.container}>
-        <h2>My previous projects</h2>
-        <div className={style.portfolios}>
-          <div className={style.portfolio}>
-            <div className={style.img} />
-            <div className={style.textContainer}>
-              <h3 className={style.portfolioTitle}>TT App</h3>
-              <p className="text">You can track your time, add projects, tasks and some notes.</p>
-              <p className="text">Later on you are also able to get an invoice and will be able to see your time sheet.</p>
-              <p className="text">You can change white- and darkmode as well as language. </p>
-            </div>
-          </div>
-          <div className={style.portfolio}>
-            <div className={style.img} />
-            <div>
-              <h3 className={style.portfolioTitle}>STUSCHENKO_DESIGN.COM</h3>
-              <p className="text">
-                I wanted to create a website where you can get to know my work as well as me and my personality and will continuous update
-                my website.{' '}
-              </p>
-              <p className="text">A website that convince, a website that after you had a look you want to meet that person. </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Layout title="portfolio" contentSpacing="m">
+        <Stack spacing="--spacing-11">
+          <Text type="h3">time tracking</Text>
+          <Text>
+            You can track your time, add projects, tasks as well as some notes.
+            <br />
+            <br /> We thought about what do the user needs to track its time best and completed the app with its best features and easy to
+            hand design.
+          </Text>
+        </Stack>
+        <Stack spacing="--spacing-11">
+          <Text type="h3">stuschenko_design.com</Text>
+          <Text>
+            I wanted to create a website where you get insight into my work as well as into my person.
+            <br />
+            <br /> To create a site that convince, a website you are willingly to visit again; this is my goal.
+          </Text>
+        </Stack>
+      </Layout>
     </Page>
   )
 }
